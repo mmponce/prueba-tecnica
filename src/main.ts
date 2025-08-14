137f5d545b25a3d7fb685587d01c56fc2af6dba6
gaetem <matias.gaete@bausch.com>
2025-08-13 20:35:33 -0400

    added new styles and index modified

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
 import './style.css';
-import './web-components/card';
-import './web-components/input';
+import './web-components/loading-spinner';
+import './web-components/search-input';
+import './web-components/info-card';
